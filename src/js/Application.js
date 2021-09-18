53c4bb5d37188c6addf15efefdd0abc340bbd042
Donyo Donev <donevdonyo39@gmail.com>
2021-09-18 14:03:05 +0300

    easing animation fro article field, with error

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,16 +1,30 @@
 import EventEmitter from "eventemitter3";
 import anime from "animejs";
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+    static get events() {
+        return {
+            READY: "ready",
+        };
+    }
 
-  constructor() {
-    super();
-    this.init();
-    this.emit(Application.events.READY);
-  }
-  init() {}
+    constructor() {
+        super();
+        this.init();
+        this.emit(Application.events.READY);
+    }
+
+    init() {
+        const article = document.querySelector('.article');
+        article.addEventListener('click', event => {
+            anime({
+                targets: article,
+                elasticity: 400,
+                translateY: 10,
+                duration: 550,
+                direction: 'alternate',
+                loop: true,
+                easing: 'spring(1, 80, 10, 0)',
+            })
+        });
+    }
 }
